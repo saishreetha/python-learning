00fdf360806ba33da59fb45fb8c555b230ab3672
saishreetha <saishreetha@gmail.com>
2026-03-29 17:30:42 +0530

    Add file

---
+++ user_input.py
@@ -0,0 +1,25 @@
+name = input("Enter your name: ")
+email = input("Enter your Email: ")
+ph = input("Enter your phone number: ")
+
+
+print("\n**********************")
+print("Name: " + name)
+print("Email: " + email)
+print("Phone number:",ph)
+print("**********************")
+
+'''
+Output:
+Enter your name: Saishreetha
+Enter your Email: saishreetha@gmail.com
+Enter your phone number: 8637464743
+
+**********************
+Name: Saishreetha
+Email: saishreetha@gmail.com
+Phone number: 8637464743
+**********************
+
+
+'''
