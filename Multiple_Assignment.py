6ec3925e01c3e874f1528ab1118acc021e75b2ca
saishreetha <saishreetha@gmail.com>
2026-03-29 14:28:54 +0530

    Add Comment to Multiple Assignment file

--- Multiple_Assignment.py
+++ Multiple_Assignment.py
@@ -1,4 +1,6 @@
 # Variables
+from numpy.distutils.lib2def import output_def
+
 name = "Hari"
 age = 18
 height = 190
@@ -7,3 +9,11 @@ print("Hi " + name)
 
 like = dislike = 2000
 print(like , dislike)
+
+''''''''
+output
+
+Hi Hari
+2000 2000
+
+''''''
