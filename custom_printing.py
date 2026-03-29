68dc35beeea9133e24e66bd1fa365fd633532746
saishreetha <saishreetha@gmail.com>
2026-03-29 16:33:55 +0530

    Add files

---
+++ custom_printing.py
@@ -0,0 +1,13 @@
+name = "Anand"
+days = 15
+year = 2021
+
+print("Dear " + name+",""\nYou have " + str(days) + " days of leave balance for this" + "\nYear("+ str(year)+")")
+
+'''
+Output:
+Dear Anand
+You have 15 days of leave balance for this
+Year(2021)
+
+'''
---
+++ type_casting.py
@@ -0,0 +1,12 @@
+#Type Casting
+otp = 784673
+otp = str(otp)
+print("your otp is " + otp)
+print(type(otp))
+
+'''
+output:
+your otp is 784673
+<class 'str'>
+
+'''
