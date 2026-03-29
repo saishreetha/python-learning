4ed70fbdb77ac2bdbfedd524004e694c00183ecd
saishreetha <saishreetha@gmail.com>
2026-03-29 14:08:37 +0530

    Add basic_string_op file

---
+++ basic_string_op.py
@@ -0,0 +1,19 @@
+#Basic string
+message = "happy birthday "
+print(message.upper())
+print(message.lower())
+print(message.title())
+print(message.capitalize())
+print(message.count("o"))
+print(message.find("o"))
+print(message.replace("p","r" ))
+name = 'Sai is'
+quote = '"beautiful"'
+print(name + quote)
+print(message.isalpha())
+print(message.isalnum())
+print(message.isidentifier())
+print(message.isspace())
+print(message.isdigit())
+print(message * 5)
+print(len(message))
