d0075b772a8824497773fe1df3adcee5fd7a2a78
saishreetha <saishreetha@gmail.com>
2026-03-30 14:28:33 +0530

    Add files to py

---
+++ Conditional_stmts.py
@@ -0,0 +1,28 @@
+#Else If
+
+pwd_correct = False
+
+if pwd_correct :
+    print("Logged In")
+else :
+    print("Incorrect Pasword")
+    print("Try Again")
+
+print("Bye")
+
+# Elif Ladder
+
+ind_score = 360
+
+if ind_score >= 350:
+    print("India will win")
+elif ind_score >= 250:
+    print("India might win")
+elif ind_score >= 150:
+    print("Aus might win")
+else :
+    print("Aus will win")
+
+print("Bye")
+
+
