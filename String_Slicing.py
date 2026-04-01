b9ab91289efdfb1953a018a04ccd5ef5a60db581
saishreetha <saishreetha@gmail.com>
2026-04-01 17:33:14 +0530

    Add this to git.

---
+++ String_Slicing.py
@@ -0,0 +1,22 @@
+#String Slicing
+
+name = "Happy"
+print(name[0])
+print("\n" + name[0:2])
+print("\n" + name[:3])
+print("\n" + name[:4])
+print("\n" + name[:5])
+
+'''
+Output :
+H
+
+Ha
+
+Hap
+
+Happ
+
+Happy
+
+'''
