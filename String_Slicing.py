b0b84fa17d608ec3229e384a88a8c76cb9fea8e6
saishreetha <saishreetha@gmail.com>
2026-04-01 17:35:04 +0530

    Add this to github.

--- String_Slicing.py
+++ String_Slicing.py
@@ -2,21 +2,16 @@
 
 name = "Happy"
 print(name[0])
-print("\n" + name[0:2])
-print("\n" + name[:3])
-print("\n" + name[:4])
-print("\n" + name[:5])
+print( name[0:2])
+print( name[:3])
+print(  name[:4])
+print( name[:5])
 
 '''
-Output :
+Output : 
 H
-
 Ha
-
 Hap
-
 Happ
-
 Happy
-
 '''
