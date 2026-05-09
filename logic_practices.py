12a4e9341891744a8f3243358fb3539ca14318b4
saishreetha <saishreetha@gmail.com>
2026-05-09 15:53:58 +0530

    Add python script for practices.

---
+++ logic_practices.py
@@ -0,0 +1,21 @@
+list = [3, 4, 6, 7, 5, 1, 8]
+
+for i in range(len(list)) :
+
+    if i == 0 :
+        list[i] >= list[i+1]
+
+    elif i == len(list) -1 :
+        list[i] >=  list[i-1]
+
+    else :
+        if list[i] >= list[i-1] and list[i] >= list[i+1] :
+                                print(list[i])
+
+
+'''
+Output :
+
+7
+
+'''
