bd097e2ad6c466b4bc55def7e6dfd62b88519676
saishreetha <saishreetha@gmail.com>
2026-05-09 14:58:19 +0530

    Add initial python script for task

---
+++ task.py
@@ -0,0 +1,12 @@
+# What to do list
+
+todo_list = []
+
+total_tasks = int(input("Enter an number: "))
+
+for i in range (total_tasks):
+    task = input("Enter a task: ")
+    todo_list.append(task)
+
+print(todo_list)
+
