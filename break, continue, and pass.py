6596e04131b5c7a1cbab0c5c6090cbff3fdd290f
saishreetha <saishreetha@gmail.com>
2026-05-29 14:03:24 +0530

    Add the python script in github

--- break, continue, and pass.py
+++ break, continue, and pass.py
@@ -1,9 +1,12 @@
+#Break
+
 print("Enter an number: \n Enter z to exit :")
 
 list =[]
 while True :
     inp = input()
     if inp == 'z' :
+#break - break the execution of the current block
         break
 
     list.append(int(inp))
@@ -23,5 +26,27 @@ Enter z to exit :
 0
 z
 [4, 8, 6, 5, 3, 2, 0]
+
+'''
+
+
+#Continue
+
+#Remove ',' from the string
+
+str = "A, B, C, D, E, F, G"
+str2= ''
+
+for i in str :
+    if i == ',' :
+# Continue - continues to the next iteration
+        continue
+    str2 = str2 + i
+
+print(str2)
+
+'''
+Output:
+A B C D E F G
 
 '''
