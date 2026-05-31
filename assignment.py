1c69a1076d1ecd8ec7c1aade06301ba67690b5e0
saishreetha <saishreetha@gmail.com>
2026-05-31 16:05:53 +0530

    Add

--- assignment.py
+++ assignment.py
@@ -13,3 +13,19 @@ Output :
 
 Found it! The number is: 80
 '''
+
+str_ip = "34,5,2,8,9"
+numbers_list = []
+
+for character in str_ip:
+    if character == ",":
+        continue
+    numbers_list.append(character)
+
+print("The list of numbers is:", numbers_list)
+
+'''
+Output :
+The list of numbers is: ['3', '4', '5', '2', '8', '9']
+
+'''
