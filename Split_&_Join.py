4c18d3244b98a616d904324754eb18be950f8b12
saishreetha <saishreetha@gmail.com>
2026-06-01 11:17:58 +0530

    Add this script to git

---
+++ Split_&_Join.py
@@ -0,0 +1,19 @@
+#Spilt and join in strings
+
+#Split :
+
+string = "abc def ghi jkl mno pqr"
+split_list = string.split(' ')
+print(split_list)
+
+#Join :
+
+str_joined = ''.join(split_list)
+(print(str_joined))
+
+
+'''
+Output :
+['abc', 'def', 'ghi', 'jkl', 'mno', 'pqr']
+abcdefghijklmnopqr
+'''
