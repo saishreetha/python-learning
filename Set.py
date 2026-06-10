d47cdb18c8dc15af36319b1835ac55bbac7de059
saishreetha <saishreetha@gmail.com>
2026-06-10 10:02:57 +0530

    github push

---
+++ Set.py
@@ -0,0 +1,22 @@
+# Set - Unique elements only we can store and it's not ordered
+# If we repeated the same colour it will print the colour only one time.
+
+cities = ["Chennai", "Madurai", "Trichy", "Coimbatore", "Salem", "Karur"]
+
+colours = {'Red','Yellow','Green','Red'}
+print (colours)
+
+colour_list = list(colours)
+print(colour_list)
+
+set(cities)
+print(cities)
+
+'''
+Output :
+
+{'Red', 'Yellow', 'Green'}
+['Red', 'Yellow', 'Green']
+['Chennai', 'Madurai', 'Trichy', 'Coimbatore', 'Salem', 'Karur']
+
+'''
